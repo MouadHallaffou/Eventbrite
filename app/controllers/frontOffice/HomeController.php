3c615d362e2ef201916bfb90b71b0dc9981f513d
Taouil-abdo <abdotaouil03@gmail.com>
2025-02-07 17:08:52 +0100

    add security class

---
+++ app/controllers/frontOffice/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\controllers\frontOffice;
+require_once __DIR__ . '/../../../vendor/autoload.php';
+
+use App\core\view;
+
+
+class HomeController{
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
